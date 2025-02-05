92e422887e0f5ffece556f9c628d8a8334dbfcca
bill <weirwang@163.com>
2025-02-05 21:01:03 +0800

    分支更新

--- git01/src/main/java/com/bill/git/HelloGit.java
+++ git01/src/main/java/com/bill/git/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("更新1 ");
         System.out.println("更新2 ");
         System.out.println("创建分支dev ");
+        System.out.println("dev分支更新 ");
     }
 }
