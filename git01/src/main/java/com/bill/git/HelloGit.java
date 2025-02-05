4ea087ea29c665b0735c77464859cc657bde42af
bill <weirwang@163.com>
2025-02-05 20:58:24 +0800

    主干添加

--- git01/src/main/java/com/bill/git/HelloGit.java
+++ git01/src/main/java/com/bill/git/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("更新1 ");
         System.out.println("更新2 ");
         System.out.println("创建分支dev ");
+        System.out.println("主干添加 ");
     }
 }
